d6078835e10120b7d0a4f3a5a9b19681bdee24f1
giauphan <giauphan012@gmail.com>
2024-01-18 09:27:21 +0700

    update route search

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\FileDestroy;
+use App\Http\Controllers\Api\FileSearchController;
 use App\Http\Controllers\Api\FileUploadController;
 use App\Http\Controllers\Api\FileViewController;
 use Illuminate\Http\Request;
@@ -23,6 +24,7 @@ Route::middleware(['auth:api', 'scopes:place-file-upload'])->group(function () {
     });
     Route::prefix('file')->group(function () {
         Route::get('show', [FileViewController::class, '__invoke']);
+        Route::get('search-file', FileSearchController::class);
         Route::post('upload', [FileUploadController::class, 'index']);
         Route::post('update', [FileUploadController::class, 'index']);
         Route::delete('delete', [FileDestroy::class, '__invoke']);
